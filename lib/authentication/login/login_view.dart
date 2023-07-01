a08ecd0ff857639543a31e01127849adcc382559
habyaad <adeomotoshan@gmail.com>
2023-07-01 18:25:46 +0100

    feat: implement the login view and viewmodel

---
+++ lib/authentication/login/login_view.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:stacked/stacked.dart';
+import '../../../utils/app_colors.dart';
+import '../../utils/common_widgets/general_button.dart';
+import '../../utils/validator.dart';
+import '../sign_up/sign_up_view.dart';
+import 'login_viewmodel.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<LoginViewModel>.reactive(
+        builder: (context, viewModel, child) => Scaffold(
+          backgroundColor: Colors.white,
+          body: SafeArea(
+            child: Form(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Column(
+                  //crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 17,
+                    ),
+                    const Text(
+                      "payCOM",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 15,
+                          color: AppColors.primaryColor),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const Text(
+                      "Welcome",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15,
+                          color: AppColors.primaryColorDark),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    SizedBox(
+                      height: 40,
+                      child: TextFormField(
+                        controller: viewModel.emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        cursorHeight: 10,
+                        decoration: InputDecoration(
+                          labelText: 'Email Address',
+                          labelStyle: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              color: AppColors.primaryColor),
+                          prefixIcon: Transform.scale(
+                              scale: 0.5,
+                              child: SvgPicture.asset(
+                                "assets/svgs/email.svg",
+                                width: 20,
+                                height: 20,
+                              )),
+                          filled: true,
+                          fillColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: AppColors.secondaryColor, width: 1),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: AppColors.secondaryColor, width: 1),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        validator: (value) =>
+                            Validator.validateEmail(value),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    SizedBox(
+                      height: 40,
+                      child: TextFormField(
+                        controller: viewModel.passwordController,
+                        cursorHeight: 10,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              color: AppColors.primaryColor),
+                          prefixIcon: Transform.scale(
+                              scale: 0.4,
+                              child: SvgPicture.asset(
+                                "assets/svgs/lock.svg",
+                              )),
+                          suffixIcon: Transform.scale(
+                              scale: 0.4,
+                              child: SvgPicture.asset(
+                                "assets/svgs/eye.svg",
+                              )),
+                          filled: true,
+                          fillColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: AppColors.secondaryColor,
+                                width: 1),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: AppColors.secondaryColor,
+                                width: 1),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        validator: (value) =>
+                            Validator.validatePassword(value),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    const Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 10,
+                            color: AppColors.primaryColorDark),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    GeneralButton(
+                      buttonColor: AppColors.primaryColorDark,
+                      buttonText: 'Sign in',
+                      textColor: Colors.white,
+                      onPressed: () {
+                        if (viewModel.formKey.currentState!.validate()) {
+                          viewModel.formKey.currentState!.save();
+                          // Do something with the validated and saved values
+                          viewModel.signIn();
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 26,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                const SignUpView()));
+                      },
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Don’t have an account?",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 12,
+                                color: AppColors.primaryColor),
+                          ),
+                          Text(
+                            "Register now",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 12,
+                                color: AppColors.primaryColorDark),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        viewModelBuilder: () => LoginViewModel());
+  }
+}
---
+++ lib/authentication/login/login_viewmodel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:stacked/stacked.dart';
+
+class LoginViewModel extends BaseViewModel{
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void signIn(){
+
+  }
+}
