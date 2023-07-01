447d61efaf5f6d144bb4fb6024bc7d08305e773d
habyaad <adeomotoshan@gmail.com>
2023-07-01 18:27:52 +0100

    feat: create app color class

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primaryColor = Color(0xFF176B87);
+  static const Color secondaryColor = Color(0xFF64CCC5);
+  static const Color primaryColorDark = Color(0xFF001C30);
+  static const Color primaryColorLight = Color(0xFFDAFFFB);
+}
