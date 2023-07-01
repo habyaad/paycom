f7863622534f65e64c50c428d6f49ef08ae4e1a1
habyaad <adeomotoshan@gmail.com>
2023-07-02 00:57:35 +0100

    feat: create api endpoints class

---
+++ lib/services/api_endpoints.dart
@@ -0,0 +1,7 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  static const baseUrl = "https://paycom.com.ng/api";
+  static const signUp = '$baseUrl/registration';
+  static const login = '$baseUrl/login';
+}
