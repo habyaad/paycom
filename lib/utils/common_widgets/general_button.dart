6136bac2fca17265c800690bd0e5bec46c50b77a
habyaad <adeomotoshan@gmail.com>
2023-07-01 18:28:34 +0100

    feat: create general button widget for re-usability

---
+++ lib/utils/common_widgets/general_button.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class GeneralButton extends StatelessWidget {
+  final Color buttonColor;
+  final String buttonText;
+  final Color textColor;
+  final FontWeight? fontWeight;
+  final double? fontSize;
+  final double? height;
+  final double? borderRadius;
+  final Border? border;
+  final void Function() onPressed;
+
+  const GeneralButton(
+      {super.key,
+        required this.buttonColor,
+        required this.buttonText,
+        required this.textColor,
+        required this.onPressed,
+        this.borderRadius,
+        this.height,
+        this.fontWeight,
+        this.fontSize,
+        this.border});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: height?? 45,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          border: border,
+          borderRadius:
+          BorderRadius.circular(borderRadius?? 5),
+          color: buttonColor,
+          //border: Border.all(width: 3, color: AppColor.primary)
+        ),
+        child: Center(
+            child: Text(
+              buttonText,
+              style: TextStyle(
+                  color: textColor,
+                  fontSize: fontSize?? 15,
+                  fontWeight: fontWeight?? FontWeight.w500),
+            )),
+      ),
+    );
+  }
+}
