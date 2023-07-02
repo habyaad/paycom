fec577eb1b3f370387b05b12c3c2af164779fb28
habyaad <adeomotoshan@gmail.com>
2023-07-02 01:01:11 +0100

    feat: add phone number validation

--- lib/utils/validator.dart
+++ lib/utils/validator.dart
@@ -1,16 +1,17 @@
 class Validator {
-  static String? validateUsername(String? username) {
-    if (username == null || username.isEmpty) {
-      return 'Username is required.';
+  Validator._();
+
+  static String? validateFullName(String? value) {
+    // Remove leading and trailing white spaces
+    value = value!.trim();
+
+    // Check if the full name consists of at least two parts
+    List<String> nameParts = value.split(' ');
+    if (nameParts.length < 2) {
+      return 'Please enter a valid full name';
     }
 
-    if (username.length < 8) {
-      return 'Username must be at least 8 characters long.';
-    }
-
-    // You can add more specific username validation rules here
-
-    return null;
+    return null; // Return null if the full name is valid
   }
 
   static String? validateEmail(String? email) {
@@ -39,5 +40,16 @@ class Validator {
     // You can add more specific password validation rules here
 
     return null;
+  }
+
+  static String? validatePhoneNumber(String? value) {
+    // Regular expression pattern for phone numbers (assumes 10 digits)
+    final RegExp regex = RegExp(r'^[0-9]{11}$');
+
+    if (!regex.hasMatch(value!)) {
+      return 'Please enter a valid 11-digit phone number';
+    }
+
+    return null; // Return null if the phone number is valid
   }
 }
