8b52f2f149f7415b1716c15296cfe06198d57e18
habyaad <adeomotoshan@gmail.com>
2023-07-02 01:00:11 +0100

    feat: create toast service

---
+++ lib/services/toast_service.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class ToastService {
+  static void show(String msg, Color color) {
+    Fluttertoast.showToast(
+        msg: msg,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 2,
+        backgroundColor: color,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  void error(String msg) {
+    ToastService.show(msg, Colors.red);
+  }
+
+  void info(String msg) {
+    ToastService.show(msg, Colors.blue);
+  }
+
+  void success(String msg) {
+    ToastService.show(msg, Colors.green);
+  }
+
+  void warning(String msg) {
+    ToastService.show(msg, Colors.orange);
+  }
+}
